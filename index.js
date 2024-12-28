e448488742c72ac5e1ce8129b9960bab62dac89d
me-mega-noob-4044 <testtrolium@gmail.com>
2024-12-28 13:59:10 -0500

    webpack stuff

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const app = express();
 const wss = new WebSocketServer({ noServer: true });
 const path = require("path");
 
-const server = app.listen(3030, () => {
-    console.log("Listening on port 3030");
+const server = app.listen(7070, () => {
+    console.log("Listening on port 7070");
+});
+
+app.get("/bundle.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "/dist/bundle.js"));
+});
+
+app.get("/style.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/style.css"));
 });
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/frontend/index.html"));
+});
+
+wss.on("connection", (ws) => {
+    console.log("rah");
 });
 
 server.on("upgrade", (request, socket, head) => {
