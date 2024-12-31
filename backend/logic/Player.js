07f1b8c91217fcea0e915dd7c2d1d23397eac3d7
me-mega-noob-4044 <testtrolium@gmail.com>
2024-12-31 10:16:18 -0500

    rename game

--- backend/logic/Player.js
+++ backend/logic/Player.js
@@ -48,7 +48,7 @@ export default class Player {
     }
 
     setName(name) {
-        this.name = name || `retard #${this.sid}`;
+        this.name = name || `combatant ${this.sid}`;
     }
 
     send(type, ...args) {
